dca71330dfc1f01e1015b5ab3d952a2f58c06389
Jason P'ng <jsjpng@gmail.com>
2018-01-26 22:51:18 -0800

    Added mongodb to server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var logger = require('morgan');
+var MongoClient = require('mongodb').MongoClient;
 var square = require('./square');
 var wiki = require('./wiki.js');
 var app = express();
@@ -8,6 +9,17 @@ var app = express();
 app.use(logger('dev'));
 console.log('The area of a square with a width of 4 is ' + square.area(4));
 
+MongoClient.connect('mongodb://jason:123@ds117128.mlab.com:17128/node_tutorial', function(err, client) {
+    if (err) throw err;
+    
+    var db = client.db('node_tutorial')
+    db.collection('mammals').find().toArray(function(err, result) {
+        if (err) throw err;
+
+        console.log(result);
+        client.close();
+    });
+});
 // Custom middleware function
 var a_middleware_function = function(req, res, next) {
     console.log("called custom func");
@@ -34,7 +46,8 @@ app.all('/secret', function(req, res, next) {
 app.use('/wiki', wiki);
 app.use('/media', express.static('public'));
 
-//Handle errors
+//Handle errors. Must be after all route calls
+//Node: To access stack trace set environment variable NODE_ENV to 'production'
 app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Something broke!');
