98cc9225c06d876043542fe39a338f6687c0e01b
Jason P'ng <jsjpng@gmail.com>
2018-01-26 22:07:26 -0800

    Added morgan logger

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
+var logger = require('morgan');
 var square = require('./square');
 var wiki = require('./wiki.js');
 var app = express();
 
+app.use(logger('dev'));
 console.log('The area of a square with a width of 4 is ' + square.area(4));
 app.get('/', function (req, res) {
     res.send('Hello World!');
