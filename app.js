c2e04f7b27c7296f2e16e7a5d45d4c9cb0068b13
Jason P'ng <jsjpng@gmail.com>
2018-01-26 21:55:33 -0800

    Added express, package.json, and square utility module

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var square = require('./square');
+var app = express();
+
+console.log('The area of a square with a width of 4 is ' + square.area(4));
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
