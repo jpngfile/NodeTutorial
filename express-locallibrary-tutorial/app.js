777284b7910bf4518493fd5448e4692c8d82b1b0
Jason P'ng <jsjpng@gmail.com>
2018-01-28 17:56:17 -0800

    Added mongodb code (doesn't work)

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -4,11 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// Database setup
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
