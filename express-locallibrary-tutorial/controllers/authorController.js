ac4a6f4ae032fd1ecc51fde6ed989e16bbe3656f
Jason P'ng <jsjpng@gmail.com>
2018-02-11 17:44:51 -0800

    Added update page to author

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -132,10 +132,62 @@ exports.author_delete_post = function(req, res) {
 
 // Display Author update form on GET
 exports.author_update_get = function (req, res) {
-    res.send('Not Implemented: Author update GET');
+
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err) }
+        if (results.author==null) { // No results
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('author_form', { title: 'Update Author', author: results.author });
+    });
 };
 
 // Handle Author update on POST
-exports.author_update_post = function(req, res) {
-    res.send('Not Implemented: Author update POST');
-};
+exports.author_update_post = [
+    
+    // Validate fields
+    body('first_name').isLength({ min: 1}).trim().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').isLength({ min: 1}).trim().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+    // Sanitize fields
+    sanitizeBody('first_name').trim().escape(),
+    sanitizeBody('family_name').trim().escape(),
+    sanitizeBody('date_of_birth').toDate(),
+    sanitizeBody('date_of_death').toDate(),
+
+    // Process request after validation and sanitization
+    (req, res, next) => {
+
+        // Extract errors
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // Render form with error messages
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        } else {
+
+            var author = new Author({
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death,
+                _id: req.params.id
+            });
+            Author.findByIdAndUpdate(req.params.id, author, {}, (function (err, theauthor) {
+                if (err) { return next(err); }
+                res.redirect(theauthor.url);
+            }));
+        }
+    }
+];
--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -31,7 +31,7 @@ AuthorSchema
 AuthorSchema
 .virtual('date_of_death_formatted')
 .get(function () {
-    return this.date_of_death ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+    return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
 });
 
 AuthorSchema
